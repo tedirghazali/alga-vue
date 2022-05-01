f421663c7d66f2afb5b9a26bdd513e4069fd71ae
Tedir Ghazali <tedirghazali@gmail.com>
2022-05-01 14:59:53 +0700

    Using .mjs file format

---
+++ src/models/countries.mjs
@@ -0,0 +1,11 @@
+import continents from './countries/continents.mjs'
+import regions from './countries/regions.mjs'
+import countries from './countries/countries.mjs'
+import divisions from './countries/divisions.mjs'
+
+export default {
+  continents,
+  regions,
+  countries,
+  divisions
+}
---
+++ src/models/countries/countries.mjs
@@ -0,0 +1,15 @@
+import NAAF from './countries/NA-AF.mjs'
+import SSAF from './countries/SS-AF.mjs'
+import MEAS from './countries/ME-AS.mjs'
+import EAAS from './countries/EA-AS.mjs'
+import CSAS from './countries/CS-AS.mjs'
+import SEAS from './countries/SE-AS.mjs'
+
+export default [
+  ...NAAF,
+  ...SSAF,
+  ...MEAS,
+  ...EAAS,
+  ...CSAS,
+  ...SEAS
+]
