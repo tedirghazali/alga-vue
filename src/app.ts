227256ab7fa8c97f3c4de46a479fc775528dec4e
Tedir Ghazali <tedirghazali@gmail.com>
2022-09-01 16:29:33 +0700

    Using Vitest now

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import useFilter from './composables/table/useFilter'
 import useSort from './composables/table/useSort'
 import usePaginate from './composables/table/usePaginate'
 
+import useDebouncedRef from './composables/ref/useDebouncedRef'
+
 import useContinents from './composables/countries/useContinents'
 import useRegions from './composables/countries/useRegions'
 import useCountries from './composables/countries/useCountries'
@@ -13,6 +15,8 @@ export {
   useFilter,
   useSort,
   usePaginate,
+  
+  useDebouncedRef,
   
   useContinents,
   useRegions,
